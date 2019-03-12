c85e3325d81e668d58ac6bc3db664949b8ba202b
Alejandro Martinez <almaja1819daw2@gmail.com>
2019-03-12 16:23:46 +0100

    Commit 1

---
+++ peupagina.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Peu de pàgina per defecte per a noves entrades 
+Plugin URI: Escriu l'adreça URL completa del teu plugin dins de Github
+Description: Crea peus de pàgina per defecte per a noves entrades
+Version: 0.1
+Author: Alejandro Martínez Jaén
+Author URI: https://github.com/almaja1819daw2
+License: GNU GPLv3 
+*/ 
+?>
